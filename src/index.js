2112219a0d6d949481ce90f8ad612e1ade5faca0
Ivan Kolesnev <i.v.kolesnev@mail.ru>
2020-10-01 19:11:34 +0300

    feat: implement function min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
+exports.min = function min(array) {
+    if (array === undefined || array[0] === undefined) {
+        return 0;
+    } else {
+        let minNumber = array[0];
 
-exports.min = function min (array) {
-  return 0;
-}
+        for (let i = 0; i < array.length; i++) {
+            if (minNumber > array[i]) {
+                minNumber = array[i];
+            }
+        }
+        return minNumber;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array[0] === undefined) {
+        return 0;
+    } else {
+        let maxNumber = array[0];
 
-exports.avg = function avg (array) {
-  return 0;
-}
+        for (let i = 0; i < array.length; i++) {
+            if (maxNumber < array[i]) {
+                maxNumber = array[i];
+            }
+        }
+        return maxNumber;
+    }
+};
+
+exports.avg = function avg(array) {
+    if (array === undefined || array[0] === undefined) {
+        return 0;
+    } else {
+        let avgNumber = 0;
+        let sum = 0;
+
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+            avgNumber = sum / array.length;
+        }
+
+        return avgNumber;
+    }
+};
